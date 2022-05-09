818241457394bc8c8a1be91dbd2fc9ee628daf6b
liujiajun <642344572@qq.com>
2022-05-09 21:02:06 +0800

    init

---
+++ src/main/java/com/stleetcode/github/io/junw/JunwApplication.java
@@ -0,0 +1,13 @@
+package com.stleetcode.github.io.junw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunwApplication.class, args);
+	}
+
+}
