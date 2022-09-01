ea62f8c6b084a8289b3d839a157826dcd66b1269
liujiajun <45444154+wo1261931780@users.noreply.github.com>
2022-09-01 09:10:19 +0800

    解决代码扫描问题

--- src/main/java/com/leetcode/eee001history/ccc001demo.java
+++ src/main/java/com/leetcode/eee001history/ccc001demo.java
@@ -1,8 +1,6 @@
 package com.leetcode.eee001history;
 
 import java.security.SecureRandom;
-import java.util.ArrayList;
-import java.util.Random;
 import java.util.Scanner;
 
 // public class ccc001demo {
@@ -26,36 +24,36 @@ import java.util.Scanner;
 // 2022年2月7日20:53:32，力扣刷题两道
 
 public class ccc001demo {
-    public static void main(String[] args) {
-        Scanner x_input = new Scanner(System.in);
-        int demo = x_input.nextInt();
-        int[] x = new int[demo];
-        // Random x1 = new Random();
-        SecureRandom x1 = new SecureRandom();
-        for (int i = 0; i < x.length; i++) {
-            int demo_num = x1.nextInt(999) + 1;
-            for (int i1 = 0; i1 < x.length; i1++) {
-                if (x[i1] == demo_num) {
-                } else {
-                    x[i] = demo_num;
-                }
-            }
-        }
-        rank(x);
-    }
+	public static void main(String[] args) {
+		Scanner x_input = new Scanner(System.in);
+		int demo = x_input.nextInt();
+		int[] x = new int[demo];
+		// Random x1 = new Random();
+		SecureRandom x1 = new SecureRandom();
+		for (int i = 0; i < x.length; i++) {
+			int demo_num = x1.nextInt(999) + 1;
+			for (int i1 = 0; i1 < x.length; i1++) {
+				if (x[i1] == demo_num) {
+				} else {
+					x[i] = demo_num;
+				}
+			}
+		}
+		rank(x);
+	}
 
-    public static void rank(int[] x) {
-        for (int i = 0, max; i < x.length - 1; i++) {
-            for (int i1 = 0; i1 < x.length - i - 1; i1++) {
-                if (x[i1] > x[i1 + 1]) {
-                    max = x[i1];
-                    x[i1] = x[i1 + 1];
-                    x[i1 + 1] = max;
-                }
-            }
-        }
-        for (int i = 0; i < x.length; i++) {
-            System.out.println(x[i]);
-        }
-    }
+	public static void rank(int[] x) {
+		for (int i = 0, max; i < x.length - 1; i++) {
+			for (int i1 = 0; i1 < x.length - i - 1; i1++) {
+				if (x[i1] > x[i1 + 1]) {
+					max = x[i1];
+					x[i1] = x[i1 + 1];
+					x[i1 + 1] = max;
+				}
+			}
+		}
+		for (int i = 0; i < x.length; i++) {
+			System.out.println(x[i]);
+		}
+	}
 }
