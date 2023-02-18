9713f06582c2a3abda30d428988741ed62454d35
junw <45444154+wo1261931780@users.noreply.github.com>
2023-02-18 13:15:42 +0800

    刷题

--- src/main/java/com/stleetcode/github/io/junw/practice/leetcode/a20230217/A047两个括号.java
+++ src/main/java/com/stleetcode/github/io/junw/practice/leetcode/a20230217/A047两个括号.java
@@ -10,6 +10,29 @@ package com.stleetcode.github.io.junw.practice.leetcode.a20230217;
  * @description
  */
 public class A047两个括号 {
+
+	/*
+	给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+
+有效字符串需满足：
+
+左括号必须用相同类型的右括号闭合。
+左括号必须以正确的顺序闭合。
+每个右括号都有一个对应的相同类型的左括号。
+示例 1：
+
+输入：s = "()"
+输出：true
+示例 2：
+
+输入：s = "()[]{}"
+输出：true
+示例 3：
+
+输入：s = "(]"
+输出：false
+	 */
+
 	public static void main(String[] args) {
 		A047两个括号 demo = new A047两个括号();
 		System.out.println(demo.isValid("){"));
---
+++ src/main/java/com/stleetcode/github/io/junw/practice/leetcode/a20230218/A049.java
@@ -0,0 +1,15 @@
+package com.stleetcode.github.io.junw.practice.leetcode.a20230218;
+
+/**
+ * Created by Intellij IDEA.
+ * Project:st-leetcode.github.io
+ * Package:com.stleetcode.github.io.junw.practice.leetcode.a20230218
+ *
+ * @author liujiajun_junw
+ * @Date 2023-02-11-26  星期六
+ * @description
+ */
+public class A049 {
+
+
+}
