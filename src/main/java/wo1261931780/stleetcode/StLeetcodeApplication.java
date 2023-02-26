20367dc8e049ef83821e2282fdf9e468f2d750cd
junw <45444154+wo1261931780@users.noreply.github.com>
2023-02-26 21:11:49 +0800

    2

---
+++ src/main/java/wo1261931780/stleetcode/StLeetcodeApplication.java
@@ -0,0 +1,13 @@
+package wo1261931780.stleetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StLeetcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StLeetcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wo1261931780/stleetcode/StLeetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package wo1261931780.stleetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StLeetcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
